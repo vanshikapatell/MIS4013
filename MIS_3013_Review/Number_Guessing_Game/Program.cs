82aa35cc44aada381b45b1be9b1de183bd4b3980
Vanshika Patel <vanshikapatel12@ou.edu>
2025-08-28 13:05:54 -0500

    worked on 3033 review and made it to step 4

--- MIS_3013_Review/Number_Guessing_Game/Program.cs
+++ MIS_3013_Review/Number_Guessing_Game/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// data_type variable_name = value
+Random r = new Random();
+
+var rando = r.Next(1, 5+1); // generate a random number between 1-5
+int guess;
+int count = 0;
+
+do
+{
+	Console.WriteLine("Please guess a number beterrn 1 and 5 << ");
+	string usersGuess = Console.ReadLine();
+	guess = Convert.ToInt32(usersGuess);
+	count++;
+
+	Console.WriteLine($"You guessed {usersGuess}");
+
+    Console.WriteLine($"Generated a random number with the value {rando}");
+    // Console.WriteLine("Generated a random number with the value " + rando + "");
+
+ //   if (rando == guess)
+	//{
+	//	Console.WriteLine("Congratulations you guessed correctly!");
+	//}
+	//else
+	//{
+	//	Console.WriteLine("Sorry you guessed incorrectly");
+	//} 
+	if (rando != guess) 
+	{
+		if (guess > rando)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("Sorry, you guessed too high");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+		else
+		{
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Sorry, you guessed too low");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+	}
+
+} while (guess != rando);
+
+Console.WriteLine($"Congratulations you guessed correctly and it only took {count} attempts!");
+
+
